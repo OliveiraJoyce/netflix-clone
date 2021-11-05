f05a455ad3f4672f960a5c31cfd21e81829bb740
OliveiraJoyce <joycezer5@gmail.com>
2021-11-05 10:50:16 -0300

    Implemetações netflix clone

---
+++ src/Tmdb.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const API_KEY = '34c5a39e77f0fe54885bf89cc3c5d30f';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Só na Netflix',
+                items: await basicFetch(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+
+            },
+
+            {
+                slug: 'comedy',
+                title: 'Filmes de Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+          
+            {
+                slug: 'action',
+                title: 'Filmes de Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+          
+            {
+                slug: 'horror',
+                title: 'Filmes de Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+            {
+                slug: 'romance',
+                title: 'Filmes de Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+        ]
+    },
+    //Pegando informações de um filme ou série especifico por id para destaque
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if (movieId) {
+            switch (type) {
+                case 'movie':
+                    // PFilme
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                    //Série 
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                default:
+                    info = null
+                    break;
+            }
+        }
+
+        return info;
+    }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+/* import React from "react";
+import './Header.css';
+
+export default () => {
+    return (
+        <header className="black">
+            <div className="header--logo">
+            <a href="/">
+                <img src= "https://i.imgur.com/zazuIqj.png" alt="Logo Netflix"/>
+            </a>
+            </div>
+            <div className="header--user">
+            <a href="/">
+                <img src= "https://i.imgur.com/kLMwfKE.jpg" alt="Usuário"/>
+            </a>
+
+            </div>
+        </header>
+    );
+ } */
+
